4a1b4cc3fb5dc4f2fec2895f1a46f89f1f30fea7
Kemrone <80783057+Kemrone@users.noreply.github.com>
2021-04-30 00:10:41 +0200

    Update MainController.java

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,34 +1,102 @@
 package controller;
 
-import appli.TEAMSProcessor;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import model.Fichier;
+import model.FichierFactory;
+import model.FichierHtml;
+import model.TEAMSProcessor;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class MainController {
-    public Label HelloWorld;
+public class MainController implements Initializable{
 
-    public void sayHelloWorld(ActionEvent actionEvent) {
+    @FXML
+    private Label depotId;
+    @FXML
+    private Label fichierId;
+    @FXML
+    private Label dateId;
+    @FXML
+    private Label heureMinId;
+    @FXML
+    private Label heureMaxId;
+    @FXML
+    private VBox dragTarget;
+    
+    private static List<File> files;
+    
+    
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
+    @FXML
+    public void handleDragFile(DragEvent event) {
+    	if(event.getDragboard().hasFiles()) {
+    		event.acceptTransferModes(TransferMode.ANY);
+    	}
+    }    
 
-
-        FileChooser fileChooser = new FileChooser();
-        File selectedFile = fileChooser.showOpenDialog(null);
-
-        // process the file, and limit periods to given time interval
-        var teamsProcessor = new TEAMSProcessor(selectedFile,"19/01/2021 �? 10:15:00", "19/01/2021 �? 11:45:00");
-/*
-        var allpeople = teamsProcessor.get_allpeople();
-        for (People people : allpeople) {
-            System.out.println( people );
-        }
-*/
-        System.out.println( teamsProcessor.toHTMLCode() );
-
+    
+    @FXML
+    public void handleFile(DragEvent event) throws FileNotFoundException {
+    	Dragboard db = event.getDragboard();
+    	
+    	files = event.getDragboard().getFiles();
+    	
+    	if (db.hasFiles()) {
+    	//fichierId.setText(db.getFiles().toString());		   		 
+    	File selectedFile = files.get(0);
+    	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    	fichierId.setText(selectedFile.getName());
+    	dateId.setText(sdf.format(selectedFile.lastModified()));
+    	}
+    	
     }
+    	public void valider(ActionEvent event) {
+    		File selectedFile = files.get(0);
+    		
+    		var t = new TEAMSProcessor(selectedFile,"19/01/2021 ﾃ? 10:15:00", "19/01/2021 ﾃ? 11:45:00");
+    		FichierFactory fichFac = new FichierFactory();
+    		Fichier fich = fichFac.getFichier("HTML", t);
+    		System.out.println(fich.getContent());
+    	}
+    	
+    	
 }
+
+
+	
+
+
+	
+    
+    
+    
+    
+    
+
